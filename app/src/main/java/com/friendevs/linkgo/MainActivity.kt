3b4eb3e25d4bdd5f307a35806a3da957cad3ceb7
NDGC14 <nicolasdgc14@gmail.com>
2026-03-04 02:31:29 -0500

    Added Opt in experimental tag to MainActivity.kt and navigation.kt

--- app/src/main/java/com/friendevs/linkgo/MainActivity.kt
+++ app/src/main/java/com/friendevs/linkgo/MainActivity.kt
@@ -5,11 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.friendevs.linkgo.navigation.Navigation
 import com.friendevs.linkgo.ui.theme.LinkGoTheme
-
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/friendevs/linkgo/navigation/navigation.kt
+++ app/src/main/java/com/friendevs/linkgo/navigation/navigation.kt
@@ -1,5 +1,6 @@
 package com.friendevs.linkgo.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -25,6 +26,7 @@ enum class Screens {
 }
 
 @Composable
+@ExperimentalMaterial3Api
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screens.Map.name) {
