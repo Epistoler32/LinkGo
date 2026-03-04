e9595f41ae00f0074661b0634380857b575a60c2
NDGC14 <nicolasdgc14@gmail.com>
2026-03-04 01:32:37 -0500

    Refined ProfileScreen.kt to better match mockups

--- app/src/main/java/com/friendevs/linkgo/screens/ProfileScreen.kt
+++ app/src/main/java/com/friendevs/linkgo/screens/ProfileScreen.kt
@@ -1,7 +1,12 @@
 package com.friendevs.linkgo.screens
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,35 +18,50 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.friendevs.linkgo.R
 import com.friendevs.linkgo.navigation.Screens
+import com.friendevs.linkgo.ui.theme.Purple40
+import com.friendevs.linkgo.ui.theme.Purple80
 
 @Composable
+@ExperimentalMaterial3Api
 fun ProfileScreen(navController: NavHostController) {
     Scaffold(
-        bottomBar = { BottomNavigationBar(navController) }
+        bottomBar = { BottomNavigationBar(navController) },
+        topBar = {UserTopAppBar()}
     ) { paddingValues ->
 
         LazyColumn(
@@ -54,33 +74,64 @@ fun ProfileScreen(navController: NavHostController) {
 
         ) {
             item(){ ProfileHeader() }
+            item() {ProfileEditRow()}
             item() { StatsRow() }
-            item { Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start){Text("MY CIRCLES", style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold) }}
-            item { CirclesSection() }
-            item { SettingsSection() }
+
         }
     }
+}
+
+@Composable
+fun ProfileEditRow(){
+        Row(
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                onClick = {},
+                Modifier.width(270.dp).height(50.dp),
+                shape = RoundedCornerShape(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    MaterialTheme
+                        .colorScheme.primary
+                )
+            )
+            {
+                Text("Editar Perfil", fontWeight = FontWeight.Bold, fontSize =18.sp)
+            }
+
+            Button(
+                onClick = {},
+                modifier = Modifier.size(50.dp),
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                ),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Share,
+                    contentDescription = "Share",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+
 }
 
 @Composable
 fun ProfileHeader() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = null,
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-        )
-
-        Text("You", style = MaterialTheme.typography.headlineSmall, fontWeight = FontWeight.Bold)
+        ProfileAvatar()
+        Text("Santiago Rodriguez", style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold, fontSize = 30.sp)
 
         Text(
-            "Downtown Manhattan",
+            "@arivers_snaps",
             style = MaterialTheme.typography.bodyMedium,
-            color = Color.Gray
+            color = MaterialTheme.colorScheme.primary, fontSize = 18.sp
         )
     }
 }
@@ -92,98 +143,44 @@ fun StatsRow() {
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
 
-        StatItem("3", "Circles",R.drawable.ic_launcher_foreground)
-        StatItem("12", "Snaps", R.drawable.ic_launcher_foreground)
-        StatItem("82%", "Battery", R.drawable.ic_launcher_foreground)
+        StatItem("3", "Fotos")
+        StatItem("12", "Amigos")
+        StatItem("14", "Circulos")
     }
 }
 
 @Composable
-fun StatItem(number: String, label: String, picRsrc: Int) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-
-
+fun StatItem(number: String, label: String) {
         Card(
-            modifier = Modifier.width(30.dp),
-            shape = RoundedCornerShape(16.dp)
-        ){
-        Image(
-            painterResource(picRsrc),
-            contentDescription = "Stat Image",
-            Modifier.size(35.dp),
-        )}
-
-        Text(number, fontWeight = FontWeight.Bold, fontSize = 25.sp)
-        Text(label, color = Color.Gray)
-    }
-}
-
-@Composable
-fun CirclesSection() {
-
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-
-        CircleCard("🔥", "Squad", "7 day streak")
-        CircleCard("🏠", "Family", "14 day streak")
-        CircleCard("💼", "Work crew", "3 day streak")
-    }
-}
-
-@Composable
-fun CircleCard(icon: String, title: String, streak: String) {
-    Card(
-        modifier = Modifier.width(120.dp).height(120.dp),
-        shape = RoundedCornerShape(16.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(12.dp).fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.width(100.dp).height(80.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors
+                (MaterialTheme.colorScheme.surface),
+            border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
         ) {
-            Text(icon, fontSize = 25.sp)
-            Text(title, fontWeight = FontWeight.Bold)
-            Text(streak, color = Color.Red, fontSize = 15.sp)
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    number, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary,
+                    fontSize = 25.sp
+                )
+                Text(label, color = Color.Gray)
+            }
         }
-    }
+
 }
 
-@Composable
-fun SettingsSection() {
-
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-
-        SettingItem("Notifications", "On")
-        SettingItem("Privacy", "")
-        SettingItem("Dark Mode", "On")
-    }
-}
-
-@Composable
-fun SettingItem(title: String, status: String) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(12.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(title)
-            Text(status, color = Color.Gray)
-        }
-    }
-}
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
-        NavigationBar {
+        NavigationBar(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface) {
+
             NavigationBarItem(
                 selected = false,
                 onClick = { navController.navigate(Screens.Map.name) },
@@ -217,3 +214,56 @@ fun BottomNavigationBar(navController: NavHostController) {
         }
 }
 
+@Composable
+@ExperimentalMaterial3Api
+fun UserTopAppBar(){
+    TopAppBar(
+        title = {Text("Perfil", fontSize = 28.sp,
+            style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold)
+                },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Default.Settings, "Profile Settings")
+            }
+        })
+}
+
+
+@Composable
+fun ProfileAvatar() {
+
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier.size(140.dp)
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.img), // tu imagen
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 4.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = CircleShape
+                )
+        )
+
+        Box(
+            modifier = Modifier
+                .size(36.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+                .border(2.dp, Color.White, CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = "Edit",
+                tint = Color.White,
+                modifier = Modifier.size(18.dp)
+            )
+        }
+    }
+}
